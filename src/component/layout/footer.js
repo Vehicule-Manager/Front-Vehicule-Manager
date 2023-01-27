4ef70dacc5906e8565fac741990716de3b8e1ea6
mansonboy <mrdaurelthomas@gmail.com>
2023-01-27 14:45:40 +0100

    create footer

--- src/component/layout/footer.js
+++ src/component/layout/footer.js
@@ -6,10 +6,37 @@ const Footer = () => {
 
     return (
         <div className='FooterBackground'>
-            <h2 className="divider line one-line " contenteditable><Icon name='car' size='large' circular inverted color='black'/></h2>
-            <p>Test</p>
-            <p>Test</p>
- </div>
+            <h2 className="divider line one-line " contenteditable><Icon name='car' size='large' circular inverted color='black' /></h2>
+            <div className='containerFooter'>
+                <div>
+                    <h3>Horraires d'ouverture garage</h3>
+                    <p>Lundi : Fermé</p>
+                    <p>Mardi : 10h - 18h</p>
+                    <p>Mercredi : 10h - 18h</p>
+                    <p>Jeudi : 10h - 18h</p>
+                    <p>Vendredi : 10h - 18h</p>
+                    <p>Samedi : 10h - 18h</p>
+                    <p>Dimanche : Fermé</p>
+
+                </div>
+                <div className='borderDivision'></div>
+                <div>
+                    <h2>Qui sommes-nous ?</h2>
+                    <h3>Auto Rentals</h3>
+                    <a className='contactsCard' href='https://goo.gl/maps/5WKCjpvjAZpQHQNQ8'>
+                        
+                        <div><Icon name='map marker alternate' size='small' circular inverted /> 10 rue belleville 60200 Compiègne</div>
+                    </a>
+                    <a className='contactsCard' href='tel:030204204'>
+                        <div> <Icon name='phone' size='small' circular inverted /> 030204204</div>
+                    </a>
+                    <a className='contactsCard' href='mailto:rien.rien@gmail.com'>
+                        <div>  <Icon name='mail' size='small' circular inverted /> rien.rien@gmail.com</div>
+                    </a>
+
+                </div>
+            </div>
+        </div>
     )
 }
 
