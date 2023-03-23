eea07d51338f621d36f8c2d33149cb4a3c776c25
MWJulien <j.fournier@mentalworks.fr>
2023-03-23 16:28:07 +0100

    fix routes

--- src/index.js
+++ src/index.js
@@ -1,52 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Leasing from './pages/Leasing';
-import Services from "./pages/services";
+import Services from './pages/Services';
 import Contact from './pages/Contact';
-import reportWebVitals from './reportWebVitals';
 import Articles from "./pages/Articles";
 import ReadArticles from "./pages/ReadArticle";
-import {createBrowserRouter, RouterProvider,} from "react-router-dom";
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/location" element={<Leasing />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/article" element={<Articles />} />
+                <Route path="/article/:id" element={<ReadArticles />} />
+            </Routes>
+        </Router>
+    </React.StrictMode>,
 
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />,
-    },
-    {
-        path: "/location",
-        element: <Leasing />,
-    },
-    {
-        path: "/services",
-        element: <Services />
-    },
-    {
-      path: "/contact",
-      element: <Contact />,
-    },
-    {
-      path : "/article",
-      element: <Articles />
-    },
-    {
-        path : "/article/:id",
-        element: <ReadArticles />
-    }
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
 );
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
